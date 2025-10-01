cc0e02f8039f1c73ba1a65c9fbd9b6dc94ebf73d
lgpecin <lgpecin@gmail.com>
2025-10-01 15:35:19 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,18 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // IMPORTANTe para GitHub Pages (project page):
-  base: mode === "production" ? "/Site-Oficial-Pecin-Design/" : "/",
-
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-
-  // Opcional: publica na pasta docs/ (facilita o Pages apontando para main/docs)
-  build: {
-    outDir: "docs",
-    emptyOutDir: true,
   },
 }));
