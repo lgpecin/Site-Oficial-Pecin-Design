cc6c99a2db62e233cd36358139165d5d51d3d2c1
lgpecin <lgpecin@gmail.com>
2025-10-02 23:48:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: mode === "production" ? "/Site-Oficial-Pecin-Design/" : "/",
   server: {
     host: "::",
     port: 8080,
